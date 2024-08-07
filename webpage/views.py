9dddf53174a9853a2ebc9ee3cd7f115d29e52282
itzSakkarinTH <itzsakkarinthyt@gmail.com>
2024-08-07 14:54:19 +0700

    config table and card

--- webpage/views.py
+++ webpage/views.py
@@ -13,3 +13,19 @@ def aboutPage(request):
 
 def contactPage(request):
     return render(request, 'contact.html')
+
+
+def table_Page(request):
+    context = {}
+    lt = list(range(0, 100))
+    context["list"] = lt
+
+    return render(request, 'table_page.html', context)
+
+
+def card_Page(request):
+    cardlist = {}
+    lt = list(range(0, 100))
+    cardlist["list"] = lt
+
+    return render(request, 'card_page.html', cardlist)
